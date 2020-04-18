b8c156e039bc04a24273f9b221e51adfd038c508
WensiPeng <wensi.peng@mail.mcgill.ca>
2020-04-18 17:26:07 +0000

    change port

--- app.js
+++ app.js
@@ -54,6 +54,6 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
 
-app.listen(3000, function(){
+app.listen((process.env.PORT || 3000), function(){
 	console.log("YelpCamp is listening ...");
 })
