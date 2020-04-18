125ff95d2442d17ee847c415bc06ae1e058d7497
WensiPeng <wensi.peng@mail.mcgill.ca>
2020-04-18 17:20:52 +0000

    change mongodb Atlas

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ var campgroundRoutes = require("./routes/campgrounds");
 var indexRoutes = require("./routes/index");
 
 //mongoose.connect("mongodb://localhost:27017/yelp-camp", {useNewUrlParser: true, useUnifiedTopology: true});
-mongoose.connect("mongodb+srv://wensi:wensi@cluster0-byy9d.mongodb.net/test?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true});
-// mongoose.connect(process.env.DATABASEURL, {useNewUrlParser: true, useUnifiedTopology: true});
+// mongoose.connect("mongodb+srv://wensi:wensi@cluster0-byy9d.mongodb.net/test?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.DATABASEURL, {useNewUrlParser: true, useUnifiedTopology: true});
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
